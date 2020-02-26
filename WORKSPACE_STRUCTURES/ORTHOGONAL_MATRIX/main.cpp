ec84fe12d199f4e3ead34917e8c58a692d8c6f26
estebanmike <57074013+estabanmike@users.noreply.github.com>
2020-02-25 22:59:34 -0600

    Update main.cpp

--- WORKSPACE_STRUCTURES/ORTHOGONAL_MATRIX/main.cpp
+++ WORKSPACE_STRUCTURES/ORTHOGONAL_MATRIX/main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <conio.h>
 
-int main(int argc, char **argv)
+using namespace std;
+
+class Node{
+  public: 
+    int data;
+    Node *up;
+    Node *righty;
+    Node *lefty;
+    Node *down;
+};
+
+int main()
 {
 	printf("hello world\n");
+    
+    getch();
 	return 0;
 }
